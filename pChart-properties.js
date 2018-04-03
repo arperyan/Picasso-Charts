6f5e27dd3431333892b3f107a488e068d43fb390
Ryan Arpe <ryan_arpe@hotmail.com>
2018-04-03 16:57:07 +0100

    Update pChart-properties.js
    Updated

--- pChart-properties.js
+++ pChart-properties.js
@@ -11,12 +11,33 @@ define([], function() {
 		uses: "measures",
 		min: 1,
 		max: 2,
+    items: {
+      barwidth: {
+        type: "number",
+				component: "slider",
+				label: "Bar Width",
+				ref: "qDef.barWidth",
+				min: 0,
+				max: 2,
+		    step: 0.5,
+				defaultValue: 1
+      },
+      MyNumProp: {
+				type: "number",
+				label: "Maximum",
+				ref: "qDef.barWidth",
+				defaultValue: 1
+			}
+    }
 	};
 
   var sorting = {
     uses: "sorting"
   };
 
+  var addons = {
+    uses: "addons"
+  };
 
 	var sectionAppearance = {
     uses: "settings",
@@ -76,10 +97,11 @@ define([], function() {
 			dimensions: dimensions,
       measures: measures,
       sorting: sorting,
+      addons: addons,
 			sectionAppearance: sectionAppearance,
       about: about
-		//	sectionInteractivity: sectionInteractivity
 
 		}
 	}
 })
+
